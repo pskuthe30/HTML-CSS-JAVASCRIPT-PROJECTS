17e29184faf63e8aca26d2fae7997d00ee75ebb3
pskuthe30 <pskuthe@gmail.com>
2021-08-21 16:45:22 -0400

    eventlistener practice

--- project4/script.js
+++ project4/script.js
@@ -57,7 +57,7 @@ secondItem.style.color = 'coral'
 //QUERYSELECTORALL
 
 let titles=document.querySelectorAll('.title');
-console.log(titles)
+// console.log(titles)
 titles[1].textContent = 'Goodday'
 let odd = document.querySelectorAll('li:nth-child(odd)')
 for (let i=0;i<odd.length;i++){
@@ -125,11 +125,58 @@ for (let i=0;i<odd.length;i++){
 // 
 // newDiv.style.fontSize = '30px';
 
-let button=document.getElementById('button').addEventListener
-('click',buttonClick);
-function buttonClick(){
-    document.getElementById('header-title').textContent='changed'
+// let button=document.getElementById('button').addEventListener
+// ('click',buttonClick);
+//  function buttonClick(e){
+    // document.getElementById('header-title').textContent='changed'
+    // document.querySelector('#main').style.backgroundColor = '#f4f4f4'
+// console.log(e.target.className);
+// console.log(e.target.classList);
+ let output = document.getElementById('output')
+// output.innerHTML = '<h3>' + e.target.id + '</h3>';
+// 
+// console.log(e.type);
+//console.log(e.clientX)
+// console.log(e.offsetX);
+// console.log(e.offsetY);
+// console.log(e.altKey);
+// }
+let box = document.getElementById('box');
+let button=document.getElementById('button');
+let select =document.querySelector('select');
+//button.addEventListener('click',runEvent);
+//button.addEventListener('dblclick',runEvent);
+ //button.addEventListener('mousedown',runEvent);
+ //button.addEventListener('mouseup',runEvent);
+// box.addEventListener('mouseenter',runEvent);
+//box.addEventListener('mouseleave',runEvent);
+//box.addEventListener('mouseover',runEvent);
+//box.addEventListener('mouseout',runEvent);
+//box.addEventListener('mousemove',runEvent);
+let itemInput = document.querySelector('input[type= "text"]');
+let form = document.querySelector('form');
+//box.addEventListener('mouseenter',runEvent);
+// itemInput.addEventListener('keydown',runEvent);
+//itemInput.addEventListener('focus',runEvent);
+//itemInput.addEventListener('blur',runEvent);
+// itemInput.addEventListener('cut',runEvent);
+// itemInput.addEventListener('paste',runEvent);
+ //itemInput.addEventListener('input',runEvent);
+//  select.addEventListener('change',runEvent);
+//select.addEventListener('input',runEvent);
+form.addEventListener('submit',runEvent);
+function runEvent(e){
+    e.preventDefault();
+console.log(e.target.value);
+//    output.innerHTML='<h3>'
+    // +e.target.value+'</h3>';
+    console.log('event type:' +e.type);
+   // output.innerHTML = '<h3>MouseX: '+e.offsetX +' </h3> <h3>MouseY: ' +e.offsetY 
+
+  // document.body.style.backgroundColor="rgb("+e.offsetX+","+e.offsetY+",40)";
 }
+
+
 
 
 
